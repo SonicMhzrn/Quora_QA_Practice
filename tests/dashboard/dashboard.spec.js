ae206019c52a02d1bbcf53adc8a5d672880b0ee3
SonicMhzrn <sonicmaharjan743@gmail.com>
2024-07-10 08:04:07 +0545

    comment_Dashboard

--- tests/dashboard/dashboard.spec.js
+++ tests/dashboard/dashboard.spec.js
@@ -1,13 +1,13 @@
-const { test, expect } = require("@playwright/test");
-const testData = require("../../Fixtures/Login.fixture.json");
-const { LoginPage } = require("../../pageObjects/dashboard.login.po");
+// const { test, expect } = require("@playwright/test");
+// const testData = require("../../Fixtures/Login.fixture.json");
+// const { LoginPage } = require("../../pageObjects/dashboard.login.po");
 
-let interceptId;
+// let interceptId;
 
-const {
-    TodayDate,
-    authenticateUser1,
-    createEntity,
-  } = require("../../utils/helper.spec");
-  const { DashboardPage } = require("../../pageObjects/dashboard.po");
+// const {
+//     TodayDate,
+//     authenticateUser1,
+//     createEntity,
+//   } = require("../../utils/helper.spec");
+//   const { DashboardPage } = require("../../pageObjects/dashboard.po");
   
